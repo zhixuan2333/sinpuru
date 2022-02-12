26e6eb9a78f3dc7747d8b7ea83c5791cf4056ce4
zhixuan <jinzhixuan666@gmail.com>
2022-02-12 19:55:41 +0900

    add workflows

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,11 +15,6 @@ const config = {
 
         // hydrate the <div id="svelte"> element in src/app.html
         target: '#svelte',
-
-        // Override http methods in the Todo forms
-        methodOverride: {
-            allowed: ['PATCH', 'DELETE']
-        }
     }
 };
 
